35e42f72eb7079f2a68432f96ebabd3d2fd1e493
Celso Marques <celso.marques82@gmail.com>
2018-09-14 12:02:44 -0300

    Include debug

--- src/with.js
+++ src/with.js
@@ -3,8 +3,14 @@
 import log from './log'
 import getValue from './get-value'
 
+const debug = Debug('koa-result-respond')
+
 export default (ctx, logOpts) => (result) => {
   log(ctx, logOpts)(result)
+
+  debug(`Result isOk = ${result.isOk()} or isError = ${result.isError()}`)
+  debug('Status', result.isOk() ? 200 : 500)
+  debug('Body', getValue(result))
   ctx.status = result.isOk() ? 200 : 500
   ctx.body = getValue(result)
 }
